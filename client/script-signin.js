a811371b01673e16732506e664fe5c39c7beef38
abc <abc@gmail.com>
2025-04-14 19:37:52 +0530

    commit last

--- client/script-signin.js
+++ client/script-signin.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (email && password) {
             try {
-                const res = await fetch('https://vss-learning-2.onrender.com', {
+                const res = await fetch('http://localhost:5000/login', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ email, password })
@@ -66,7 +66,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (name && email && password) {
             try {
-                const res = await fetch('https://vss-learning-2.onrender.com', {
+                const res = await fetch('http://localhost:5000/register', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ name, email, password })
